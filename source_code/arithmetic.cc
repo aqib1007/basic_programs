9216d6e78f7b2b2aaabc9b94485d3fcfedbb652e
aqib1007 <35717627+aqib1007@users.noreply.github.com>
2018-05-31 00:40:05 +0530

    Add files via upload

---
+++ source_code/arithmetic.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double a,first,second;
+    cin>>first;
+    cin>>second;
+    cout<<first+second<<endl;
+    cout<<first-second<<endl;
+    cout<<first*second<<endl;
+a=(first/second);
+    cout<<a<<endl;
+    return 0;
+}
